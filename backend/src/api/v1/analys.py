408ff4544a5a07c5e4e2e05f511d21e872672f53
duck <gkarpov076@gmail.com>
2025-09-05 23:24:14 +0000

    added endpoints

--- backend/src/api/v1/analys.py
+++ backend/src/api/v1/analys.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from typing import List
 from core.pool import get_conn
 
 
@@ -7,6 +8,15 @@ analys = APIRouter(prefix="/analys")
 
 @analys.post("/", status_code=200)
 def analys_one(
+    query: str,
+    conn=Depends(get_conn)  
+):
+    return "Hello, World!"
+
+
+@analys.post("/bulk", status_code=200)
+def analys_one(
+    queries: List[str],
     conn=Depends(get_conn)  
 ):
     return "Hello, World!"
